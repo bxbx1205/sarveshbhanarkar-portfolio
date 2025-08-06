170271d75ed9c957ed4505d4c20bf7d72e6099c9
Sarvesh Bhanarkar <srbhanarkar05@gmail.com>
2025-08-06 20:32:31 +0530

    new file:   app/components/ProfileCard.css 	new file:   app/components/ProfileCard.jsx 	modified:   app/globals.css 	modified:   app/page.js 	new file:   public/avatar.png

--- app/page.js
+++ app/page.js
@@ -1,11 +1,42 @@
-import Image from "next/image";
 import React from "react";
 import Navbar from "./components/Navbar";
+import ProfileCard from "./components/ProfileCard";
 
 export default function Home() {
   return (
-    <>
-    <Navbar />
-    </>
+    <div className="min-h-screen bg-black flex flex-col">
+      <Navbar />
+
+      {/* Main content container - left section centered */}
+      <main className="flex-1 flex p-8 relative">
+        <div className="w-2/5 flex items-center justify-center">
+          <ProfileCard
+            name="Sarvesh Bhanarkar"
+            title="Student/Developer"
+            handle="sarveshbhanarkar"
+            status="AFK"
+            contactText="IDK" // this will be ignored/unused
+            avatarUrl="/avatar.png" // ✅ fixed path
+            showUserInfo={true}
+            enableTilt={true}
+            enableMobileTilt={false}
+          />
+        </div>
+        
+        {/* Green vertical line at 40% from left */}
+        <div className="absolute top-0 bottom-0 w-px bg-green-400" style={{ left: '40%' }}></div>
+        
+        {/* Right section */}
+        <div className="flex-1"></div>
+      </main>
+
+      {/* Footer */}
+      <footer className="border-t border-green-400 p-4">
+        <div className="flex justify-between items-center font-mono text-sm">
+          <div className="text-green-400">bxbx1205@portfolio:~$</div>
+          <div className="text-green-400">2025-08-06 14:59:52</div>
+        </div>
+      </footer>
+    </div>
   );
 }
