ea3c6f5a590f7fe9e3e68e1466a6eb8dc0d2c6e4
Sarvesh Bhanarkar <srbhanarkar05@gmail.com>
2025-08-22 16:07:25 +0530

    added speed and analysis

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import ProfileCard from "./components/ProfileCard";
 import Terminal from "./components/Terminal";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
@@ -53,6 +54,7 @@ export default function Home() {
       </main>
 
       <Footer className="mt-auto" />
+      <Analytics/>
       <SpeedInsights/>
     </div>
     
